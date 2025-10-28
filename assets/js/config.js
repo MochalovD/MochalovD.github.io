4ebd49bffc3b7684be2575870298ba5692593617
Dmitrii Mochalov <wowalmeirah@gmail.com>
2025-10-28 22:16:37 +0100

    october update, professional exp added

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const SITE = {
   name: "Dmitrii Mochalov",
-  role: "Game Designer / Technical Game Designer",
+  role: "Level Designer / Game Designer",
   email: "jpdomitori@gmail.com",
   linkedin: "https://www.linkedin.com/in/dmitrii-mochalov/",
   telegram: "https://t.me/BetaEmperor"
